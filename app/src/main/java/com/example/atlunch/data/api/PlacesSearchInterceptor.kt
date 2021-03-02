31b708ac9c379d0c937ca0716fc7c71a3afa6cb6
david calle <david.calle@yum.com>
2021-03-02 09:25:44 -0800

    search api update
    added hardcoded values to PlacesSearchInterceptor.kt as companion object
    updated IRestaurantRepo.kt to use LatLng instead of userlocation

--- app/src/main/java/com/example/atlunch/data/api/PlacesSearchInterceptor.kt
+++ app/src/main/java/com/example/atlunch/data/api/PlacesSearchInterceptor.kt
@@ -1,6 +1,5 @@
 package com.example.atlunch.data.api
 
-import com.example.atlunch.data.models.NetworkResponse
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -8,13 +7,16 @@ class PlacesSearchInterceptor(private val key : String): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         val url = request.url().newBuilder().addQueryParameter("key",key)
-            .addQueryParameter("type", NetworkResponse.TYPE)
-            .addQueryParameter("rankby", NetworkResponse.RANKBY)
+            .addQueryParameter("type", TYPE)
+            .addQueryParameter("rankby", RANKBY)
             .build()
         request  = request.newBuilder().url(url).build()
         return chain.proceed(request)
 
     }
-
+    companion object{
+        const val TYPE = "restaurant"
+        const val RANKBY = "distance"
+    }
 
 }
--- app/src/main/java/com/example/atlunch/data/repositories/IRestaurantRepo.kt
+++ app/src/main/java/com/example/atlunch/data/repositories/IRestaurantRepo.kt
@@ -3,14 +3,13 @@ package com.example.atlunch.data.repositories
 
 import com.example.atlunch.common.ApiResponseWrapper
 import com.example.atlunch.data.models.Favorite
-import com.example.atlunch.data.models.NetworkResponse
-import com.example.atlunch.data.models.UserLocation
+import com.example.atlunch.data.models.Restaurant
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Response
 
 interface IRestaurantRepo {
     suspend fun addFavorite(id: String)
     suspend fun removeFavorite(id : String)
     suspend fun getFavorites(): List<Favorite>
-    suspend fun getRestaurants(userLocation: UserLocation, query : String?): Flow<ApiResponseWrapper<Response<NetworkResponse>>>
+    suspend fun getRestaurants(userLocation: LatLng, query : String?): Flow<ApiResponseWrapper<List<Restaurant>>>
 }
