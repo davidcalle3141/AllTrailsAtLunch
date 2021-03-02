eef3f556fde0799b5d49937ce777879860909ce3
david calle <david.calle@yum.com>
2021-03-02 09:30:11 -0800

    Restaurant.kt
    made field nullable
    added logic to make the isFavorite field immutable after setting value

--- app/src/main/java/com/example/atlunch/data/models/Restaurant.kt
+++ app/src/main/java/com/example/atlunch/data/models/Restaurant.kt
@@ -3,23 +3,28 @@ package com.example.atlunch.data.models
 data class Restaurant(
     val place_id: String,
     val name: String,
-    val plusCode: String,
-    val rating: Double,
-    val price_level: Int,
-    val photos: List<Photo>,
-    val opening_hours: OpenHours,
-    val type : String,
+    val plusCode: String?,
+    val rating: Double?,
+    val price_level: Int?,
+    val photos: List<Photo>?,
+    val opening_hours: OpenHours?,
+    val type : String?,
     val geometry: Geometry,
-    var isFavorite: Boolean,
-    val coverPhotoUrl : String
+    private var isFavorite: Boolean?,
+    val coverPhotoUrl : String?
 ){
     fun getPriceLevel(): String {
+        if(price_level == null) return ""
         var price = ""
         for (i in 0 until price_level){
             price+= "$"
         }
         return price
     }
+    fun setFavorite(value : Boolean){
+        if(isFavorite == null) isFavorite = value
+    }
+    fun getFavorite() = isFavorite?: false
 }
 
 data class Geometry (
@@ -33,4 +38,4 @@ data class Location (
     val lng : Double
 )
 data class Photo(val photo_reference: String)
-data class OpenHours(val open_now: Boolean)
+data class OpenHours(val open_now: Boolean?)
