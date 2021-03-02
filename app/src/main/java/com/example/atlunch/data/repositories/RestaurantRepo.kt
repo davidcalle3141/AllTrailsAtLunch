47408209151025d9530e25878205c2e3c4681386
david calle <david.calle@yum.com>
2021-03-02 09:23:21 -0800

    restaurant repo favorites and api update
    updataed the get restaurants method to return a list of restaurants
    started running database logic in a new thread
    added logging

--- app/src/main/java/com/example/atlunch/data/repositories/RestaurantRepo.kt
+++ app/src/main/java/com/example/atlunch/data/repositories/RestaurantRepo.kt
@@ -1,21 +1,22 @@
 package com.example.atlunch.data.repositories
 
+import android.util.Log
 import com.example.atlunch.common.ApiResponseWrapper
 import com.example.atlunch.data.api.PlacesApiService
 import com.example.atlunch.data.database.FavoriteDao
 import com.example.atlunch.data.models.Favorite
-import com.example.atlunch.data.models.NetworkResponse
-import com.example.atlunch.data.models.UserLocation
+import com.example.atlunch.data.models.Restaurant
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
-import retrofit2.Response
 
 class RestaurantRepo(
     private val favoriteDao: FavoriteDao,
     private val placesApi: PlacesApiService
 ) : IRestaurantRepo {
+    private val TAG = RestaurantRepo::class.java.simpleName
     private val favoritesSet = hashSetOf<String>()
     private var initFavorites: Boolean = false
 
@@ -33,35 +34,61 @@ class RestaurantRepo(
         list.forEach { favoritesSet.add(it.place_id) }
     }
 
+    fun isFavorite(id: String) = favoritesSet.contains(id)
+
+    suspend fun toggleFavorite(id: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            try {
+                if (favoritesSet.contains(id)) removeFavorite(id)
+                else addFavorite(id)
+                return@withContext true
+            } catch (e: Exception) {
+                Log.e(RestaurantRepo::class.java.simpleName, e.message, e)
+            }
+            return@withContext false
+        }
+
+    }
+
     override suspend fun addFavorite(id: String) {
         favoritesSet.add(id)
         favoriteDao.addFavorite(Favorite(id))
+        Log.d(TAG, "writing new favorite to db")
     }
 
     override suspend fun removeFavorite(id: String) {
         favoritesSet.remove(id)
         favoriteDao.deleteFavorite(id)
+        Log.d(TAG, "removing favorite from dp")
+
     }
 
     override suspend fun getFavorites() = favoriteDao.getAll()
 
 
     override suspend fun getRestaurants(
-        userLocation: UserLocation,
+        userLocation: LatLng,
         query: String?,
-    ): Flow<ApiResponseWrapper<Response<NetworkResponse>>> = flow {
+    ): Flow<ApiResponseWrapper<List<Restaurant>>> = flow {
         emit(ApiResponseWrapper.Loading)
         try {
-            emit(
-                ApiResponseWrapper.Success(
-                    placesApi.getRestaurantsFromPlaces(
-                        userLocation.toPlacesRequestString(),
-                        query ?: ""
-                    )
-                )
-            )
+            Log.d(TAG, "making API call")
+            val restaurants = placesApi.getRestaurantsFromPlaces(
+                "${userLocation.latitude},${userLocation.longitude}",
+                query ?: ""
+            ).body()?.results ?: listOf()
+
+            restaurants.forEach {
+                it.setFavorite(isFavorite(it.place_id))
+            }
+
+            emit(ApiResponseWrapper.Success(restaurants))
+
+
         } catch (e: Exception) {
             emit(ApiResponseWrapper.Error(e))
+            Log.e(TAG,"Error retrieving places from API", e)
+
         }
 
 
